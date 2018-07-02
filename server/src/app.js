8bc9c8a5c7f1b1e1336b94ee4aeed03b2824ea3d
Yuriy Zelensky <thelenskyy@gmail.com>
2018-07-02 11:43:10 +0300

    first step

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,19 @@
 
-const express = require ('express')
-const bodyParser = require ('body-parser')
-const cors = require ('cors')
-const morgan = require ('morgan')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const app = express()
+const app = express();
 
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/greating', (req,res) => {
-    res.send({
-        message: 'Hello muthafuker!'
-    })
-})
+app.post('/register', (req, res) => {
+	res.send({
+		message: `Hello muthafuker! from ${req.body.email}`
+	});
+});
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8081);
